8f8696770f3107181469725878be22abb16cd8f5
Sobol Eduard <92293088+Edward1an@users.noreply.github.com>
2026-08-15 13:53:22 +0200

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res{};
+        unordered_map<string, vector<string>> m{};
+        array<int, 26> arr{};
+        for(const auto str : strs){
+            for(const auto chr : str){
+                arr[chr-97]++;
+            }
+            string temp;
+            temp.reserve(50);
+            for(auto i{0};i<26;i++){
+                if(arr[i]>0){
+                    temp += to_string(i) + ":" + to_string(arr[i]) + ",";
+                    arr[i]=0;
+                }
+            }
+            if(!m.contains(temp)){
+                m[temp] = {};
+            }
+            m[temp].push_back(str);
+        }
+        for(const auto & p : m){
+            res.push_back(p.second);
+        }
+        return res;
+    }
+};
